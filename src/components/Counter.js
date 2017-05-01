ece2f295c9783e6dbac18234dee1ec2d97e52c93
George Spake <gpspake@gmail.com>
2017-04-30 22:33:08 -0500

    create counter component

---
+++ src/components/Counter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  render() {
+    return (
+      <div className="Counter">Counter</div>
+    );
+  }
+}
+
+export default Counter;
