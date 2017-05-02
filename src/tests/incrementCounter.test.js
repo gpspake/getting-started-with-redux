5629c81c531928de85be57d22d0a25d3f5b5811c
George Spake <gpspake@gmail.com>
2017-05-02 12:06:36 -0500

    create increment counter test and initial increment counter function

---
+++ src/tests/incrementCounter.test.js
@@ -0,0 +1,16 @@
+import deepFreeze from 'deep-freeze';
+
+const incrementCounter = (list, index) => {
+  return list;
+};
+
+it('adds counter', () => {
+
+  const listBefore = [0, 10, 20];
+
+  deepFreeze(listBefore);
+
+  const listAfter = [0, 11, 20];
+
+  expect( incrementCounter(listBefore, 1) ).toEqual( listAfter );
+});
