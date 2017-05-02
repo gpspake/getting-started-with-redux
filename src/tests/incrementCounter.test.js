971683747584dba6fe24f1d5087166cac26683b6
George Spake <gpspake@gmail.com>
2017-05-02 12:18:11 -0500

    refactor increment counter using es6 implicit return syntax

--- src/tests/incrementCounter.test.js
+++ src/tests/incrementCounter.test.js
@@ -1,12 +1,6 @@
 import deepFreeze from 'deep-freeze';
 
-const incrementCounter = (list, index) => {
-  return [
-    ...list.slice(0, index),
-    list[index] + 1,
-    ...list.slice(index + 1)
-  ];
-};
+const incrementCounter = (list, index) => [...list.slice(0, index), list[index] + 1, ...list.slice(index + 1)];
 
 it('adds counter', () => {
 
