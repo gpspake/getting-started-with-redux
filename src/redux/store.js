4209ea0a7c39089617742ef6ac2d2d3bf67d08ce
George Spake <gpspake@gmail.com>
2017-05-02 10:38:37 -0500

    enable redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux'
 import { counter } from './reducers'
 
-const store = createStore(counter);
+const store = createStore(counter, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store
